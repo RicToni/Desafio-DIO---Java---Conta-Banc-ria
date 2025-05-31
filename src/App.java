4cad49ff5dd0ce2ea278474f724c4ec879eed862
RicToni <ricardotonholli@gmail.com>
2025-05-31 16:07:40 -0300

    App.java criado e fluxo inicial ja funcionando

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("OLá cliente, por favor insira seu nome: ");
+    String nome = scanner.nextLine();
+    
+    System.out.println("Ola " + nome + ", agora por favor digite o número da agência: ");
+    String agencia = scanner.nextLine();
+
+    System.out.println("Ola " + nome + ", agora por favor digite o número da sua conta: ");
+    int numero = Integer.parseInt(scanner.nextLine());
+
+    System.out.println("Ola " + nome + ", agora por favor digite o valor do seu saldo inicial: ");
+    float saldo = Float.parseFloat(scanner.nextLine());
+    
+    Conta conta1 = new Conta(numero, agencia, nome, saldo);
+
+    System.err.println(conta1.toString());
+    
+
+}
+
+    
+
 }
